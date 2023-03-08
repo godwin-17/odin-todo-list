776487f50d092aaf6ef7f82c2d9b7e41dff322c6
Godwin <agblezeg@gmail.com>
2023-03-08 17:02:14 +0100

    Add active button function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import '../src/style.css';
+
+const asideBtns = document.querySelectorAll(".aside-btn");
+
+asideBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+
+    asideBtns.forEach(btns => {
+      btns.classList.remove("active-btn");
+    });
+
+    btn.classList.add("active-btn");
+    
+  });
+});
