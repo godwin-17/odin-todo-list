cbbe777ca09fe9377f5e740fb2dd8af394cba7f4
Godwin <agblezeg@gmail.com>
2023-03-08 12:21:48 +0100

    Add CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -26,6 +27,11 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'style.css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'src/images', to: 'images'}
+      ]
     })
   ],
   devtool: 'inline-source-map',
