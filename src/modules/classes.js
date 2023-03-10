76f50a42762264c39cfb6259587a34d6f74ace8a
Godwin <agblezeg@gmail.com>
2023-03-10 19:07:50 +0100

    Create Todos and Projects classes

---
+++ src/modules/classes.js
@@ -0,0 +1,41 @@
+class Todo {
+  static counter = 0;
+  constructor (title, description, date, project) {
+    this.id = ++Todo.counter;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.project = project;
+  }
+}
+
+class Project {
+  constructor (name) {
+    this.name = name;
+    this.todos = [];
+  }
+  
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  removeTodo(todo) {
+    const index = this.todos.indexOf(todo);
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
+
+class AllProjects {
+  constructor () {
+    this.name = "All Projects";
+    this.projects = [];
+  }
+
+  addProject(project) {
+    this.projects.push(project);
+  }
+}
+
+export {Todo, Project, AllProjects};
