b8c5c158ba429dcfd8e2d2472939926fc37a8571
Godwin <agblezeg@gmail.com>
2023-03-10 10:38:27 +0100

    Add project DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import '../src/style.css';
 
 const asideBtns = document.querySelectorAll(".aside-btn");
+const addProject = document.querySelector("#add-project");
+const projectPopup = document.querySelector("#project-popup");
+const closeProject = document.querySelector("#close-popup");
 
 asideBtns.forEach(btn => {
   btn.addEventListener("click", () => {
@@ -13,3 +16,17 @@ asideBtns.forEach(btn => {
     
   });
 });
+
+addProject.addEventListener("click", () => {
+  projectPopup.style.display = "block";
+});
+
+projectPopup.addEventListener("submit", e => {
+  e.preventDefault();
+  // -------------------------------
+  projectPopup.style.display = "none";
+});
+
+closeProject.addEventListener("click", () => {
+  projectPopup.style.display = "none";
+})
