8c36acf91a8d54b3530d54d927f381c563d21949
Godwin <agblezeg@gmail.com>
2023-03-10 19:08:22 +0100

    Create DOM Manipulation file

---
+++ src/modules/dom.js
@@ -0,0 +1,16 @@
+const asideBtns = document.querySelectorAll(".aside-btn");
+const addProject = document.querySelector("#add-project");
+const projectPopup = document.querySelector("#project-popup");
+const closeProjectPopup = document.querySelector("#close-popup");
+const addTodo = document.querySelector("#add-todo");
+const todoPopup = document.querySelector("#todo-popup");
+const closeTodoPopup = document.querySelector("#close-todo");
+const calendarProjects = document.querySelector(".calendar-projects");
+const sidebar = document.querySelector("aside");
+const main = document.querySelector("main");
+const projectInput = document.querySelector("#project");
+const todoInputTitle = document.querySelector("#title");
+const todoInputDescription = document.querySelector("#description");
+const todoInputDate = document.querySelector("#date");
+
+export { asideBtns, addProject, projectPopup, closeProjectPopup, addTodo, todoPopup, closeTodoPopup, calendarProjects, sidebar, main, projectInput, todoInputTitle, todoInputDescription, todoInputDate };
