42898ced5bc01809543d620ae8ee67050e07a3cf
Godwin <agblezeg@gmail.com>
2023-03-11 13:41:18 +0100

    Set up default Project and Todo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,49 @@ import {asideBtns, addProject, projectPopup, closeProjectPopup, addTodo, todoPop
 
 const allProjects = new AllProjects(); // Creating an Array of all projects
 
+// Setting up default Project and Todo
+const DefaultProject = new Project("Default");
+const DefaultTodo = new Todo("Example", "This is an example of a todo.", "2023-03-11", DefaultProject);
+DefaultProject.addTodo(DefaultTodo);
+allProjects.addProject(DefaultProject);
+
+
 let currentProject = null;
+
+function displayDefaultProject() {
+  const newProject = document.createElement("div");
+  newProject.classList.add("project-item");
+  newProject.innerHTML = `${DefaultProject.name}`;
+
+  setCurrentProject(DefaultProject);
+  calendarProjects.insertAdjacentElement("afterend", newProject);
+}
+
+displayDefaultProject();
+
+function displayDefaultTodo(defaultTodo) {
+  main.innerHTML += `
+  <div class="todo-item">
+    <div class="todo-details">
+      <div class="todo-title">${defaultTodo.title}</div>
+      <div class="todo-description">${defaultTodo.description}</div>
+    </div>
+
+    <div class="todo-details-2">
+      <input type="date" name="todo-date" class="todo-date" value=${defaultTodo.date}>
+
+      <div class="delete-todo">
+        &times;
+      </div>
+    </div>
+  </div>
+  `;
+}
+displayDefaultTodo(DefaultTodo);
+
+function setCurrentProject (projectName) {
+  currentProject = projectName.name;
+}
 
 // Set Data Attribute
 function setDataAttribute() {
@@ -54,7 +96,7 @@ projectPopup.addEventListener("submit", e => {
   newProject.classList.add("project-item");
   newProject.innerHTML = `${myProject.name}`;
 
-  currentProject = myProject.name;
+  setCurrentProject(myProject);
 
   calendarProjects.insertAdjacentElement("afterend", newProject);
   console.log("Project Created", myProject);
