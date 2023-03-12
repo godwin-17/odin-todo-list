85ded8334d10fd1b89bb37e18d49fff4ced54c96
Godwin <agblezeg@gmail.com>
2023-03-12 14:13:00 +0100

    Add main text and todo container DOM elements

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -8,9 +8,11 @@ const closeTodoPopup = document.querySelector("#close-todo");
 const calendarProjects = document.querySelector(".calendar-projects");
 const sidebar = document.querySelector("aside");
 const main = document.querySelector("main");
+const todoItemsContainer = document.querySelector(".todo-items-container");
+const mainTitleText = document.querySelector(".main-title-text");
 const projectInput = document.querySelector("#project");
 const todoInputTitle = document.querySelector("#title");
 const todoInputDescription = document.querySelector("#description");
 const todoInputDate = document.querySelector("#date");
 
-export { asideBtns, addProject, projectPopup, closeProjectPopup, addTodo, todoPopup, closeTodoPopup, calendarProjects, sidebar, main, projectInput, todoInputTitle, todoInputDescription, todoInputDate };
+export { asideBtns, addProject, projectPopup, closeProjectPopup, addTodo, todoPopup, closeTodoPopup, calendarProjects, sidebar, main, todoItemsContainer, mainTitleText, projectInput, todoInputTitle, todoInputDescription, todoInputDate };
